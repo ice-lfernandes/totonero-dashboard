9b31160c302bde8bda8f03264df982a510d358bc
ice_lfernandes <ice_lfernandes@uolinc.com>
2022-04-07 15:12:52 -0300

    refresh automatico e ordenação pelo score

--- src/app/view/dashboard-view/dashboard-view.component.ts
+++ src/app/view/dashboard-view/dashboard-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardBffService } from 'src/app/service/dashboard-bff.service';
 import { Match } from '../../service/model/match.model';
 
+const intervalSeconds = 10000
+
 @Component({
   selector: 'app-dashboard-view',
   templateUrl: './dashboard-view.component.html',
@@ -11,18 +13,22 @@ export class DashboardViewComponent implements OnInit {
 
   matchs: Array<Match> = []
   clazz: String = `card text-center shadow p-3 mb-5 rounded card-presentation-game border-secondary `
+  interval: any
 
 
   constructor(private service: DashboardBffService) { }
 
   ngOnInit(): void {
     this.updateMatches()
+    this.interval = setInterval(() => {
+      this.updateMatches();
+    }, intervalSeconds);
   }
 
 
   async updateMatches() {
     const result: Array<Match> = await this.service.getMatches();
-    this.matchs = result
+    this.matchs = result.slice().sort((n1, n2) => n2.score - n1.score)
   }
 
   changeCardByScore(score: number): string {
@@ -35,7 +41,7 @@ export class DashboardViewComponent implements OnInit {
     }
   }
 
-  
+
 }
 
 
