26d2f51fc745779bced2c3ce1a6bde4761094969
ice_lfernandes <ice_lfernandes@uolinc.com>
2022-04-08 08:18:46 -0300

    refinamento tela

--- src/app/view/dashboard-view/dashboard-view.component.ts
+++ src/app/view/dashboard-view/dashboard-view.component.ts
@@ -25,7 +25,6 @@ export class DashboardViewComponent implements OnInit {
     }, intervalSeconds);
   }
 
-
   async updateMatches() {
     const result: Array<Match> = await this.service.getMatches();
     this.matchs = result.slice().sort((n1, n2) => n2.score - n1.score)
