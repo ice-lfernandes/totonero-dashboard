cff35632d9d07038ce32b1e58d534067560aba3a
ice_lfernandes <ice_lfernandes@uolinc.com>
2022-05-05 20:20:14 -0300

    avanço nos menus de fiiltros e tabela de jogos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ import { FormFilterComponent } from './view/dashboard-view/form-filter/form-filt
       return {
         cache: new InMemoryCache(),
         link: httpLink.create({
-          uri: 'http://totonero-dashbord-service.us-east-1.elasticbeanstalk.com/graphql',
+          uri: 'http://localhost:8090/graphql',
         }),
       };
     },
